67d571b6a7770889b0385542e54702beb9d7fbd3
padmakondreddy <kondreddypadma705@gmail.com>
2025-03-04 12:39:35 +0530

    first message

---
+++ src/main/java/Selenium63/Selenium63/App.java
@@ -0,0 +1,13 @@
+package Selenium63.Selenium63;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
